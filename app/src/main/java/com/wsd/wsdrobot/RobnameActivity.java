6c07d769f133b47e3bee7bf554a4a7e1e5b17283
彭棟輝 <hui-MacBook@pengdonghuideMacBook-Pro.local>
2017-08-18 10:45:35 +0800

    完成增加语音合成

--- app/src/main/java/com/wsd/wsdrobot/RobnameActivity.java
+++ app/src/main/java/com/wsd/wsdrobot/RobnameActivity.java
@@ -2,40 +2,43 @@ package com.wsd.wsdrobot;
 
 import android.content.ComponentName;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import im.delight.android.ddp.ResultListener;
-import im.delight.android.ddp.Meteor;
-import im.delight.android.ddp.MeteorCallback;
-import im.delight.android.ddp.db.memory.InMemoryDatabase;
+import android.os.Environment;
 import android.speech.RecognitionListener;
 import android.speech.SpeechRecognizer;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.baidu.speech.VoiceRecognitionService;
 import com.baidu.tts.auth.AuthInfo;
 import com.baidu.tts.client.SpeechError;
 import com.baidu.tts.client.SpeechSynthesizer;
 import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.SynthesizerTool;
 import com.baidu.tts.client.TtsMode;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import im.delight.android.ddp.Meteor;
+import im.delight.android.ddp.MeteorCallback;
+import im.delight.android.ddp.db.memory.InMemoryDatabase;
 
-public class RobnameActivity extends AppCompatActivity implements MeteorCallback, RecognitionListener, SpeechSynthesizerListener {
+
+public class RobnameActivity extends AppCompatActivity implements MeteorCallback, RecognitionListener, View.OnClickListener, SpeechSynthesizerListener {
 
     private static final String TAG = "pdh";
     private Meteor mMeteor;
@@ -44,7 +47,19 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
     private SpeechRecognizer speechRecognizer;
     //语音合成客户端
     private SpeechSynthesizer speechSynthesizer;
+    private String mSampleDirPath;
+    private static final String SAMPLE_DIR_NAME = "baiduTTS";
+    private static final String SPEECH_FEMALE_MODEL_NAME = "bd_etts_speech_female.dat";
+    private static final String SPEECH_MALE_MODEL_NAME = "bd_etts_speech_male.dat";
+    private static final String TEXT_MODEL_NAME = "bd_etts_text.dat";
+    private static final String LICENSE_FILE_NAME = "temp_license";
+    private static final String ENGLISH_SPEECH_FEMALE_MODEL_NAME = "bd_etts_speech_female_en.dat";
+    private static final String ENGLISH_SPEECH_MALE_MODEL_NAME = "bd_etts_speech_male_en.dat";
+    private static final String ENGLISH_TEXT_MODEL_NAME = "bd_etts_text_en.dat";
 
+    private static final int PRINT = 0;
+    private static final int UI_CHANGE_INPUT_TEXT_SELECTION = 1;
+    private static final int UI_CHANGE_SYNTHES_TEXT_SELECTION = 2;
 
     private EditText editText;
     private WebView webView;
@@ -56,81 +71,35 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
         setContentView(R.layout.robname);
         initMeteor();
         initSpeechRecognizer();
-        startTTS();
         initWidgetListen();
+        initialEnv();
+        initialTts();
     }
 
-    private void initMeteor(){
+    private void initMeteor() {
         Meteor.setLoggingEnabled(true);
-        mMeteor = new Meteor(this,config.meteorSocketUrl,new InMemoryDatabase());
+        mMeteor = new Meteor(this, config.meteorSocketUrl, new InMemoryDatabase());
         mMeteor.addCallback(this);
         mMeteor.connect();
     }
 
-    private void initSpeechRecognizer(){
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this,new ComponentName(this,VoiceRecognitionService.class));
+    private void initSpeechRecognizer() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this, new ComponentName(this, VoiceRecognitionService.class));
         speechRecognizer.setRecognitionListener(this);
     }
 
-    // 初始化语音合成客户端并启动
-    private void startTTS() {
-//        //获取assets路径
-//        InputStream assetsFile = getClass().getClassLoader().getResourceAsStream("assets/bd_etts_speech_female.dat");
-//        //getResource("/assets/bd_etts_speech_female.dat");
-//        Log.d("地址", "file: " + assetsFile);
-
-        // 获取语音合成对象实例
-        speechSynthesizer = SpeechSynthesizer.getInstance();
-        // 设置context
-        speechSynthesizer.setContext(this);
-        // 设置语音合成状态监听器
-        speechSynthesizer.setSpeechSynthesizerListener(this);
-        // 设置在线语音合成授权，需要填入从百度语音官网申请的api_key和secret_key
-        speechSynthesizer.setApiKey("btRQQDLbWOgkUPegmGUej119", "bIe96UAAi8MT6BfqQPljYPpTWZzcVWOL");
-        // 设置离线语音合成授权，需要填入从百度语音官网申请的app_id
-        speechSynthesizer.setAppId("10018798");
-        // 设置语音合成文本模型文件
-        speechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, "file:///android_asset/bd_etts_text.dat");
-        // 设置语音合成声音模型文件
-        speechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, "file:///android_asset/bd_etts_speech_female.dat");
-        // 设置语音合成声音授权文件
-        speechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_LICENCE_FILE, "file:///android_asset/temp_license");
-        // 获取语音合成授权信息
-        AuthInfo authInfo = speechSynthesizer.auth(TtsMode.MIX);
-        // 判断授权信息是否正确，如果正确则初始化语音合成器并开始语音合成，如果失败则做错误处理
-        if (authInfo.isSuccess()) {
-            speechSynthesizer.initTts(TtsMode.MIX);
-            speechSynthesizer.speak("百度语音合成示例程序正在运行");
-        } else {
-            // 授权失败
-            Log.d(TAG, "startTTS: 授权失败");
-        }
-
-    }
-
-    private void startVoiceRecognizer(){
+    private void startVoiceRecognizer() {
         speechRecognizer.cancel();
         Intent intent = new Intent();
         speechRecognizer.startListening(intent);
     }
 
-    private  void initWidgetListen(){
+    private void initWidgetListen() {
 
         editText = (EditText) findViewById(R.id.resText);
 
-        Button startListenBtn = (Button) findViewById(R.id.StartListenBtn);
-        startListenBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startVoiceRecognizer();
-            }
-        });
-
-        Button startSpeak = (Button) findViewById(R.id.StartListenBtn);
-        startSpeak.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                speak();
-            }
-        });
+        findViewById(R.id.StartListenBtn).setOnClickListener(this);
+        findViewById(R.id.speak).setOnClickListener(this);
 
         webView = (WebView) findViewById(R.id.webView);
         //WebView加载web资源
@@ -149,22 +118,24 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
         settings.setJavaScriptEnabled(true);
     }
 
-    private void speak() {
-        String text = this.editText.getText().toString();
-        Log.d(TAG, "speak: " + text);
-        //需要合成的文本text的长度不能超过1024个GBK字节。
-        if (TextUtils.isEmpty(editText.getText())) {
-            editText.setText(text);
-        }
-        int result = this.speechSynthesizer.speak(text);
-        if (result < 0) {
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //开始收听
+            case R.id.StartListenBtn:
+                startVoiceRecognizer();
+                break;
+            //开始说话
+            case R.id.speak:
+                speak();
+                break;
         }
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         speechRecognizer.destroy();
+        speechSynthesizer.release();
         super.onDestroy();
     }
 
@@ -186,30 +157,29 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
     @Override
     public void onException(Exception e) {
         System.out.println("Exception");
-        if(e!=null){
+        if (e != null) {
             e.printStackTrace();
         }
     }
 
     @Override
     public void onDataAdded(String collectionName, String documentID, String newValuesJson) {
-        System.out.println("Data added to <"+collectionName+"> in document <"+documentID+">");
-        System.out.println("    Added: "+newValuesJson);
+        System.out.println("Data added to <" + collectionName + "> in document <" + documentID + ">");
+        System.out.println("    Added: " + newValuesJson);
     }
 
     @Override
     public void onDataChanged(String collectionName, String documentID, String updatedValuesJson, String removedValuesJson) {
-        System.out.println("Data changed in <"+collectionName+"> in document <"+documentID+">");
-        System.out.println("    Updated: "+updatedValuesJson);
-        System.out.println("    Removed: "+removedValuesJson);
+        System.out.println("Data changed in <" + collectionName + "> in document <" + documentID + ">");
+        System.out.println("    Updated: " + updatedValuesJson);
+        System.out.println("    Removed: " + removedValuesJson);
     }
-
 
 
     //语音识别相关
     @Override
     public void onDataRemoved(String collectionName, String documentID) {
-        System.out.println("Data removed from <"+collectionName+"> in document <"+documentID+">");
+        System.out.println("Data removed from <" + collectionName + "> in document <" + documentID + ">");
     }
 
     @Override
@@ -228,7 +198,7 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
     public void onRmsChanged(float rmsdB) {
         System.out.println("onRmsChanged " + rmsdB);
         Log.d(TAG, "音量: " + rmsdB);
-        mMeteor.call("listenVolume.updateVolume", new Object[] {"vol", rmsdB});
+        mMeteor.call("listenVolume.updateVolume", new Object[]{"vol", rmsdB});
     }
 
     @Override
@@ -312,7 +282,7 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
             String sn = jsonObj.getJSONObject("result").getString("sn");
             //ddp 调用后台方法
             //'listenStatus.updateSpeakStatus'(msgid, status, sayMessage, errorMsg)
-            mMeteor.call("listenStatus.updateSpeakStatus", new Object[] {corpus_no, res_type, nbest.get(0), err_no});
+            mMeteor.call("listenStatus.updateSpeakStatus", new Object[]{corpus_no, res_type, nbest.get(0), err_no});
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -333,35 +303,186 @@ public class RobnameActivity extends AppCompatActivity implements MeteorCallback
 
     }
 
+
     /**
      * 百度语音合成
      */
-    public void onError(String arg0, SpeechError arg1) {
-        // 监听到出错，在此添加相关操作
+    private void initialEnv() {
+        if (mSampleDirPath == null) {
+            String sdcardPath = Environment.getExternalStorageDirectory().toString();
+            mSampleDirPath = sdcardPath + "/" + SAMPLE_DIR_NAME;
+        }
+        makeDir(mSampleDirPath);
+        copyFromAssetsToSdcard(false, SPEECH_FEMALE_MODEL_NAME, mSampleDirPath + "/" + SPEECH_FEMALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, SPEECH_MALE_MODEL_NAME, mSampleDirPath + "/" + SPEECH_MALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, TEXT_MODEL_NAME, mSampleDirPath + "/" + TEXT_MODEL_NAME);
+        copyFromAssetsToSdcard(false, LICENSE_FILE_NAME, mSampleDirPath + "/" + LICENSE_FILE_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_SPEECH_FEMALE_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_SPEECH_FEMALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_SPEECH_MALE_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_SPEECH_MALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_TEXT_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_TEXT_MODEL_NAME);
     }
 
-    public void onSpeechFinish(String arg0) {
-        // 监听到播放结束，在此添加相关操作
+    private void makeDir(String dirPath) {
+        File file = new File(dirPath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
     }
 
-    public void onSpeechProgressChanged(String arg0, int arg1) {
-        // 监听到播放进度有变化，在此添加相关操作
+    /**
+     * 将sample工程需要的资源文件拷贝到SD卡中使用（授权文件为临时授权文件，请注册正式授权）
+     *
+     * @param isCover 是否覆盖已存在的目标文件
+     * @param source
+     * @param dest
+     */
+    private void copyFromAssetsToSdcard(boolean isCover, String source, String dest) {
+        File file = new File(dest);
+        if (isCover || (!isCover && !file.exists())) {
+            InputStream is = null;
+            FileOutputStream fos = null;
+            try {
+                is = getResources().getAssets().open(source);
+                String path = dest;
+                fos = new FileOutputStream(path);
+                byte[] buffer = new byte[1024];
+                int size = 0;
+                while ((size = is.read(buffer, 0, 1024)) >= 0) {
+                    fos.write(buffer, 0, size);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    public void onSpeechStart(String arg0) {
-        // 监听到合成并播放开始，在此添加相关操作
+    private void initialTts() {
+        this.speechSynthesizer = SpeechSynthesizer.getInstance();
+        this.speechSynthesizer.setContext(this);
+        this.speechSynthesizer.setSpeechSynthesizerListener(this);
+        // 文本模型文件路径 (离线引擎使用)
+        this.speechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, mSampleDirPath + "/"
+                + TEXT_MODEL_NAME);
+        // 声学模型文件路径 (离线引擎使用)
+        this.speechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, mSampleDirPath + "/"
+                + SPEECH_FEMALE_MODEL_NAME);
+        // 本地授权文件路径,如未设置将使用默认路径.设置临时授权文件路径，LICENCE_FILE_NAME请替换成临时授权文件的实际路径，仅在使用临时license文件时需要进行设置，如果在[应用管理]中开通了正式离线授权，不需要设置该参数，建议将该行代码删除（离线引擎）
+        // 如果合成结果出现临时授权文件将要到期的提示，说明使用了临时授权文件，请删除临时授权即可。
+        this.speechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_LICENCE_FILE, mSampleDirPath + "/"
+                + LICENSE_FILE_NAME);
+        // 请替换为语音开发者平台上注册应用得到的App ID (离线授权)
+        this.speechSynthesizer.setAppId("10018798"/*这里只是为了让Demo运行使用的APPID,请替换成自己的id。*/);
+        // 请替换为语音开发者平台注册应用得到的apikey和secretkey (在线授权)
+        this.speechSynthesizer.setApiKey("btRQQDLbWOgkUPegmGUej119",
+                "bIe96UAAi8MT6BfqQPljYPpTWZzcVWOL"/*这里只是为了让Demo正常运行使用APIKey,请替换成自己的APIKey*/);
+        // 发音人（在线引擎），可用参数为0,1,2,3。。。（服务器端会动态增加，各值含义参考文档，以文档说明为准。0--普通女声，1--普通男声，2--特别男声，3--情感男声。。。）
+        this.speechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "3");
+        // 设置Mix模式的合成策略
+        this.speechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
+        // 授权检测接口(只是通过AuthInfo进行检验授权是否成功。)
+        // AuthInfo接口用于测试开发者是否成功申请了在线或者离线授权，如果测试授权成功了，可以删除AuthInfo部分的代码（该接口首次验证时比较耗时），不会影响正常使用（合成使用时SDK内部会自动验证授权）
+        AuthInfo authInfo = this.speechSynthesizer.auth(TtsMode.MIX);
+
+        if (authInfo.isSuccess()) {
+            Log.d(TAG, "auth success");
+        } else {
+            String errorMsg = authInfo.getTtsError().getDetailMessage();
+            Log.d(TAG, "auth failed errorMsg=" + errorMsg);
+        }
+
+        // 初始化tts
+        speechSynthesizer.initTts(TtsMode.MIX);
+        // 加载离线英文资源（提供离线英文合成功能）
+        int result =
+                speechSynthesizer.loadEnglishModel(mSampleDirPath + "/" + ENGLISH_TEXT_MODEL_NAME, mSampleDirPath
+                        + "/" + ENGLISH_SPEECH_FEMALE_MODEL_NAME);
+        Log.d(TAG, "loadEnglishModel result=" + result);
+
+        //打印引擎信息和model基本信息
+        printEngineInfo();
     }
 
-    public void onSynthesizeDataArrived(String arg0, byte[] arg1, int arg2) {
-        // 监听到有合成数据到达，在此添加相关操作
+    private void speak() {
+        String text = this.editText.getText().toString();
+        //需要合成的文本text的长度不能超过1024个GBK字节。
+        if (TextUtils.isEmpty(editText.getText())) {
+            text = "欢迎使用百度语音合成SDK,百度语音为你提供支持。";
+            editText.setText(text);
+        }
+        int result = this.speechSynthesizer.speak(text);
+        if (result < 0) {
+            Log.d(TAG, "error,please look up error code in doc or URL:http://yuyin.baidu.com/docs/tts/122 ");
+        }
     }
 
-    public void onSynthesizeFinish(String arg0) {
-        // 监听到合成结束，在此添加相关操作
+    private void pause() {
+        this.speechSynthesizer.pause();
     }
 
-    public void onSynthesizeStart(String arg0) {
-        // 监听到合成开始，在此添加相关操作
+    /**
+     * 打印引擎so库版本号及基本信息和model文件的基本信息
+     */
+    private void printEngineInfo() {
+        Log.d(TAG, "EngineVersioin=" + SynthesizerTool.getEngineVersion());
+        Log.d(TAG, "EngineInfo=" + SynthesizerTool.getEngineInfo());
+        String textModelInfo = SynthesizerTool.getModelInfo(mSampleDirPath + "/" + TEXT_MODEL_NAME);
+        Log.d(TAG, "textModelInfo=" + textModelInfo);
+        String speechModelInfo = SynthesizerTool.getModelInfo(mSampleDirPath + "/" + SPEECH_FEMALE_MODEL_NAME);
+        Log.d(TAG, "speechModelInfo=" + speechModelInfo);
     }
 
+    @Override
+    public void onSynthesizeStart(String s) {
+
+    }
+
+    @Override
+    public void onSynthesizeDataArrived(String s, byte[] bytes, int i) {
+
+    }
+
+    @Override
+    public void onSynthesizeFinish(String s) {
+
+    }
+
+    @Override
+    public void onSpeechStart(String s) {
+
+    }
+
+    @Override
+    public void onSpeechProgressChanged(String s, int i) {
+
+    }
+
+    @Override
+    public void onSpeechFinish(String s) {
+
+    }
+
+    @Override
+    public void onError(String s, SpeechError speechError) {
+
+    }
 }
